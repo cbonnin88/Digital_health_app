5cc938acbc7f7a2401cd585adb26929e74e2d6d3
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-08-04 10:28:13 +0200

    Update eurohealth_dashboard.py

--- eurohealth_dashboard.py
+++ eurohealth_dashboard.py
@@ -17,9 +17,20 @@ dataset = 'dbt_health_app'
 
 st.set_page_config(page_title='EuroHealth Product Dashboard',layout='wide')
 
-import google.auth
+gcp_credentials = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"],
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+}
 
-credentials, _ = google.auth.default()
+credentials = credentials = service_account.Credentials.from_service_account_info(gcp_credentials)
 client = bigquery.Client(credentials=credentials, project=project_id)
 
 @st.cache_data
